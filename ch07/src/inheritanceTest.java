87fa66002e980818c4796ec4039d2f18411f628f
rudqls007 <rudqls007@gmail.com>
2023-05-15 15:36:12 +0900

    단일상속, Object Class

--- ch07/src/inheritanceTest.java
+++ ch07/src/inheritanceTest.java
@@ -1,4 +1,4 @@
-class MyPoint {
+class MyPoint extends Object { // 컴파일러의 의한 기본 값으로 오브젝트 메서드를 상속 받음.
 	int x;
 	int y;
 }
@@ -10,7 +10,7 @@ class MyPoint {
  * }
  */
 
-class Circle {
+class Circle extends Object { // 컴파일러의 의한 기본 값으로 오브젝트 메서드를 상속 받음.
 	MyPoint c = new MyPoint();	// 참조변수의 초기화
 	int r;
 }
@@ -20,12 +20,18 @@ public class inheritanceTest {
 	public static void main(String[] args) {
 		
 		Circle c = new Circle();
+		Circle c2 = new Circle();
 		c.c.x = 1;
 		c.c.y = 2;
 		c.r = 3;
 		System.out.println("c.c.x = " + c.c.x);
 		System.out.println("c.c.y = " + c.c.y);
 		System.out.println("c.r = " + c.r);
+		
+		
+		// Object 메서드
+		System.out.println(c.toString());
+		System.out.println(c2.toString());
 	}
 
 }
