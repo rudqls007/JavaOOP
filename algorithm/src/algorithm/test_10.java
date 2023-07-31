2b855fde4dd3901bbeafbc00c4d2c6a301f2f008
rudqls007 <rudqls007@gmail.com>
2023-07-31 22:47:54 +0900

    algorithm - 가위바위보

--- algorithm/src/algorithm/test_10.java
+++ algorithm/src/algorithm/test_10.java
@@ -2,7 +2,9 @@ package algorithm;
 
 import java.util.Scanner;
 
+// Java - 자바 알고리즘 문제풀이 (17) 소수(에라토스테네스 체)
 public class test_10 {
+	
 	public static void main(String[] args) {
 //		자연수 N이 입력되면 1부터 N까지의 소수의 개수를 출력하는 프로그램을 작성하세요. 
 //		만약 20이 입력되면 1부터 20까지의 소수는 2, 3, 5, 7, 11, 13, 17, 19로 총 8개입니다.
