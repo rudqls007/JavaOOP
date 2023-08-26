07c9cc2fe1947df3b29961997ad6332e5b9b84d4
rudqls007 <rudqls007@gmail.com>
2023-08-26 20:59:31 +0900

    Java - 자바 알고리즘 문제풀이 (11) 문자열 압축

---
+++ algorithm/src/algorithm_/Test_14.java
@@ -0,0 +1,43 @@
+package algorithm_;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+
+// Java - 자바 알고리즘 문제풀이 (11) 문자열 압축
+
+public class Test_14 {
+	public static void main(String[] args) {
+		
+		// 문자열 압축 KKHSSSSSSSE
+		
+		ArrayList<String> al = new ArrayList<>();
+		
+		Scanner sc = new Scanner(System.in);
+		String answer = sc.nextLine();
+		
+		int arrNum = 1;
+		for (int i = 0; i < answer.length(); i++) {
+			al.add(answer.substring(i, arrNum));	
+			arrNum++;	
+		}
+		
+		Set<String> alSet = new HashSet<String>(al); // set 컬렉션프레임 워크
+		
+		for (String str : alSet) {
+			
+			int num = Collections.frequency(al, str); // 컬렉션 안에서 객체가 몇 번 등장했는지 알려주는 메소드
+													  // 등장한 갯수를 리턴
+			
+			if (num == 1) {
+				System.out.print(str);
+			} else {
+				System.out.print(str + num);
+			}
+
+			}				
+	}	
+}
