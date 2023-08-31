bc4c0fbb7941ba275f227f2bd28ae817105472b3
rudqls007 <rudqls007@gmail.com>
2023-08-31 23:39:36 +0900

    Java - 자바 알고리즘 문제풀이 (5) 특정문자 뒤집기

---
+++ algorithm/src/algorithm_/Test_09.java
@@ -0,0 +1,52 @@
+package algorithm_;
+
+import java.util.Scanner;
+
+// Java - 자바 알고리즘 문제풀이 (5) 특정문자 뒤집기
+
+public class Test_09 {
+
+	
+	public static void main(String[] args) {
+		// 특정문자 빼고 뒤집기
+
+		  Scanner sc=new Scanner(System.in);
+		    
+		    String answer = sc.next();
+		    
+		    // 단어를 잘라서 배열에 넣기
+		    char[] arr = answer.toCharArray();
+		    
+		    int up = 0;
+		    int dw = arr.length-1;
+		    
+			while (up < dw) {
+				char upper = arr[up];
+				char downer = arr[dw];
+				
+				// 문자가 소문자인지
+				if (!Character.isAlphabetic(upper)) {
+					up++;
+					
+					// 문자가 대문자인지
+				} else if (!Character.isAlphabetic(downer)) {
+					dw--;
+					
+				} else {
+					// 알파벳일때 교환
+					char tmp = arr[up];
+					arr[up] = arr[dw];
+					arr[dw] = tmp;
+					up++;
+					dw--;
+				}
+			}
+
+			// char[]을 String 으로
+			String change = String.valueOf(arr);
+			System.out.println(change);
+
+		}
+
+
+}
