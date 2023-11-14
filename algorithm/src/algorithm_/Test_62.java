1d2df5dbac8db330bae56425d82a26f57f99cd6c
rudqls007 <rudqls007@gmail.com>
2023-11-14 23:28:46 +0900

    [코테/JAVA] 월간 코드 챌린지 시즌2 : 2개 이하로 다른 비트

--- algorithm/src/algorithm_/Test_62.java
+++ algorithm/src/algorithm_/Test_62.java
@@ -7,6 +7,9 @@ package algorithm_;
 public class Test_62 {
 
 	
+	
+	// 풀이 1
+	
 	class Solution {
 	    public long[] solution(long[] numbers) {
 	        return findNumberByBit(numbers);
@@ -38,6 +41,45 @@ public class Test_62 {
 	        return output;
 	    }
 	}
+
+	
+	
+	
+	
+	// 풀이 2
+	
+	class Solution2 {
+	    public long[] solution(long[] numbers) {
+	        return findNumberByBit(numbers);
+	    }
+	 
+	    public long[] findNumberByBit(long[] numbers) {
+	        // Output Instance
+	        long[] output = new long[numbers.length];
+	        
+	        for (int i = 0; i < numbers.length; i++) {
+	            long number = numbers[i];
+	            long tmp = number;
+	            int cnt = 0;
+	            
+	            while (tmp > 0) {
+	                if(tmp%2 == 0)
+	                    break;
+	                tmp /= 2;
+	                cnt++;
+	            }
+	 
+	            if (cnt == 0)
+	                number++;
+	            else 
+	                number = number + (long)Math.pow(2, cnt) - (long)Math.pow(2, cnt-1);
+	            
+	            output[i] = number;
+	        }
+	        return output;
+	    }
+	}
+
 
 	/* https://limreus.tistory.com/153  */
 	
